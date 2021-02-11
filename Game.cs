82eb65487be338f61f2242911ef184bb5931aec7
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-11 14:31:36 -0600

    Added choice for player to choose to play vs.AI or vs.Player or to Exit the game. Keeping player in the Menu until they type a valid Response.

--- Game.cs
+++ Game.cs
@@ -20,11 +20,44 @@ namespace RPSLS
         // Member Methods(CAN do)
         public void RunGame()
         {
-            Console.WriteLine("Prepare to play Rock Paper Scissors Lizard Spock!\n");
-            Console.WriteLine("It's like Rock Paper Scissors with the following rules:");
-            Console.WriteLine("Rock crushes Scissors \nScissors cuts Paper \n Paper covers Rock \n Rock crushes Lizard" +
-                "Lizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \n" +
-                "Paper disproves Spock \n Spock vaporizes Rock \n");
+            bool keepRunningMenu = true;
+            string userInput;
+
+            while (keepRunningMenu)
+            {
+                Console.WriteLine("Prepare to play Rock Paper Scissors Lizard Spock!\n");
+
+                Console.WriteLine("It's like Rock Paper Scissors with the following rules:");
+                Console.WriteLine("Rock crushes Scissors \nScissors cuts Paper \n Paper covers Rock \n Rock crushes Lizard" +
+                    "Lizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \n" +
+                    "Paper disproves Spock \n Spock vaporizes Rock \n");
+
+                Console.WriteLine("Enter the number for which game mode you want to play:");
+                Console.WriteLine("1: Player vs. AI \n2: Player vs. Player \n0: Exit Game");
+                userInput = Console.ReadLine();
+
+                if (userInput == "1")
+                {
+                    
+                }
+                else if(userInput == "2")
+                {
+
+                }
+                else if (userInput == "0")
+                {
+                    keepRunningMenu = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option,try again\n");
+                }
+
+            }
+            
+
+
+
 
         }
 
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace RPSLS
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
         }
     }
 }
