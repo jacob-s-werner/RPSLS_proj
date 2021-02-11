f291514a840ff2d978d76bbcccf9dd613450718f
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-11 14:09:12 -0600

    Created RunGame method in Game Class, added a welcome screen displaying the rules of the game.

--- Game.cs
+++ Game.cs
@@ -18,7 +18,15 @@ namespace RPSLS
         }
 
         // Member Methods(CAN do)
+        public void RunGame()
+        {
+            Console.WriteLine("Prepare to play Rock Paper Scissors Lizard Spock!\n");
+            Console.WriteLine("It's like Rock Paper Scissors with the following rules:");
+            Console.WriteLine("Rock crushes Scissors \nScissors cuts Paper \n Paper covers Rock \n Rock crushes Lizard" +
+                "Lizard poisons Spock \nSpock smashes Scissors \nScissors decapitates Lizard \nLizard eats Paper \n" +
+                "Paper disproves Spock \n Spock vaporizes Rock \n");
 
+        }
 
     }
 }
