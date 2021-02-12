bf6d63d81927e3c060b16c503a275cc40b40e585
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-12 14:21:05 -0600

    Added Gesture class: has name variable, and a list of Gestures that it beats, along with a list of Gestures it loses to.

---
+++ Gesture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Gesture
+    {
+        string name;
+        List<Gesture> losesTo;
+        List<Gesture> beats;
+
+        public Gesture(string name)
+        {
+            this.name = name;
+
+            losesTo = new List<Gesture>();
+            beats = new List<Gesture>();
+
+        }
+
+        
+
+
+    }
+}
